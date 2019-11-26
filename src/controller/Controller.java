5a7bde650e7180c0c3e241f1c75da4d077285802
Vitalii Mamchur <vitaliiimamchur@gmail.com>
2019-11-26 14:27:56 +0200

    First commit

---
+++ src/controller/Controller.java
@@ -0,0 +1,24 @@
+package controller;
+
+import model.Model;
+import model.Note;
+import view.View;
+
+import java.util.Scanner;
+
+public class Controller {
+    private Model model;
+    private View view;
+    private Note note;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.model = model;
+    }
+
+    public void processUser() {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
---
+++ src/controller/Main.java
@@ -0,0 +1,9 @@
+package controller;
+
+import model.Model;
+import view.View;
+
+public class Main {
+    Controller controller = new Controller(new Model(), new View());
+    //controller.processUser();
+}
